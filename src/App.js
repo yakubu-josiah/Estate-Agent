85c552fc5a7c2b88c4022d7c2b5449ac409f4d4e
yakubu-josiah <jossysmothest007@gmail.com>
2023-06-12 18:09:14 +0100

    creating pages and routes && adding auth pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import ForgotPassword from "./pages/auth/ForgotPassword";
 
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={ < Home /> } />
+          <Route path="/profile" element={ < Profile /> } />
+          <Route path="/offers" element={ < Offers /> } />
+          <Route path="/sign-in" element={ < SignIn /> } />
+          <Route path="/sign-up" element={ < SignUp /> } />
+          <Route path="/forgot-password" element={ < ForgotPassword /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
