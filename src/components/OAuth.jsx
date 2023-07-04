8fe0a267fb13f7a08bfae8167b7a1ccccf0c0d8f
yakubu-josiah <jossysmothest007@gmail.com>
2023-07-04 02:27:56 +0100

    fix: OAuth bg color and hover effect

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
-import { AiFillTwitterCircle } from "react-icons/ai";
-
 
 export default function OAuth() {
   return (
-    <div className='flex justify-center text-4xl'>
-        <FaFacebookF className='rounded-full bg-gray-200 p-1 cursor-pointer'/>
-        <FcGoogle className='rounded-full bg-gray-200 p-1 ml-2 cursor-pointer'/>
-        <FaTwitter className='rounded-full bg-gray-200 p-1 ml-2 cursor-pointer'/>
+    <div className="flex justify-center text-4xl">
+      <FaFacebookF className="rounded-full p-1 cursor-pointer bg-orange-100 hover:bg-orange-200 transition-all" />
+      <FcGoogle className="rounded-full p-1 ml-2 cursor-pointer bg-orange-100 hover:bg-orange-200 transition-all" />
+      <FaTwitter className="rounded-full p-1 ml-2 cursor-pointer bg-orange-100 hover:bg-orange-200 transition-all" />
     </div>
-  )
+  );
 }
