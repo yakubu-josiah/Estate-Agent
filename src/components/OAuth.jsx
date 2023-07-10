d493860e3f5dc76cd76affb33ad90f19dc1ac3b1
yakubu-josiah <jossysmothest007@gmail.com>
2023-07-10 15:58:54 +0100

    feat: OAuthFacebook signing up user functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,38 +1,75 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
 import { Timestamp, doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
   const nav = useNavigate();
-  async function OAuthGoogle() {
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const OAuthFacebook = async () => {
+    let provider = new FacebookAuthProvider();
     try {
       let account = await signInWithPopup(auth, provider);
       let user = account.user;
-      const dataRef = await getDoc(doc(db, "users", user.uid));
-      if (!dataRef.exists()) {
+      let dataRef = doc(db, "users", user.uid);
+      let dataSnap = await getDoc(dataRef);
+      if (!dataSnap.exists()) {
         await setDoc(dataRef, {
-          name: user.displayName,
+          username: user.displayName,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          timestamp: Timestamp.fromDate(new Date()),
+          photo: user.photoURL,
+        });
+        console.log(user);
+        nav("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const OAuthGoogle = async () => {
+    let provider = new GoogleAuthProvider();
+    try {
+      let account = await signInWithPopup(auth, provider);
+      let user = account.user;
+      let dataRef = doc(db, "users", user.uid);
+      let dataSnap = await getDoc(dataRef);
+      if (!dataSnap.exists()) {
+        await setDoc(dataRef, {
+          username: user.displayName,
           email: user.email,
           phoneNumber: user.phoneNumber,
           photo: user.photoURL,
           timestamp: Timestamp.fromDate(new Date()),
         });
+        console.log(user);
         nav("/");
       }
     } catch (error) {
-      console.log(error);
-      console.log("Couldn't authorize with Google");
+      if (error.code === "auth/popup-closed-by-user") {
+        // User closed the authentication popup
+        console.log("Authentication popup closed by the user");
+      } else {
+        // Other error occurred, handle it accordingly
+        console.log("Error during authentication:", error);
+      }
     }
-  }
+  };
   return (
     <div className="flex justify-center text-4xl">
-      <FaFacebookF className="rounded-full p-1 cursor-pointer bg-orange-100 hover:bg-orange-200 transition-all" />
+      <FaFacebookF
+        className="rounded-full p-1 cursor-pointer bg-orange-100 hover:bg-orange-200 transition-all"
+        onClick={OAuthFacebook}
+      />
       <FcGoogle
         className="rounded-full p-1 ml-2 cursor-pointer bg-orange-100 hover:bg-orange-200 transition-all"
         onClick={OAuthGoogle}
