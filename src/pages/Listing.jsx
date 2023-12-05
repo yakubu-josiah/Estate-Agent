beaf8298663a4199f9df85b74355e65525ba71c7
yakubu-josiah <jossysmothest007@gmail.com>
2023-12-05 19:54:33 +0100

    feat: share button and functionality on Listings

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebaseConfig";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FaShare } from "react-icons/fa";
 import {
   Navigation,
   Pagination,
@@ -17,6 +18,7 @@ import Loader from "../components/Loader";
 export default function Listing() {
   const [listing, setListing] = useState(null);
   const [isLoading, setisLoading] = useState(true);
+  const [sharedLink, setSharedLink] = useState(false);
   const param = useParams();
 
   useEffect(() => {
@@ -68,6 +70,22 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="fixed top-[13%] right-[3%] z-10 bg-[#fff7d1] cursor-pointer border-2 border-purple-600 rounded-full w-12 h-7 flex justify-center items-center text-md text-purple-600 sm:h-12 sm:top-[40%] sm:text-2xl ">
+        <FaShare
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            setSharedLink(true);
+            setTimeout(() => {
+              setSharedLink(false);
+            }, 2000);
+          }}
+        />
+      </div>
+      {sharedLink && (
+        <div className="fixed top-[20%] right-[3%] font-semibold border-2 border-purple-300 rounded-md bg-[#fff7d1] z-10 p-1 text-purple-600 text-sm sm:top-[48%] sm:text-md ">
+          Link Copied
+        </div>
+      )}
     </main>
   );
 }
