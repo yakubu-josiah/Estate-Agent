051cb35e36c019b569900b625e1e4b2d79c2a116
Yakubu Josiah <jossysmothest007@gmail.com>
2024-01-17 23:55:13 +0100

    feat: structuring and optimizing Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { doc, getDoc } from "firebase/firestore";
+import { IoCloseCircleOutline } from 'react-icons/io5';
+import { db } from '../firebaseConfig';
+import { toast } from 'react-toastify';
+
+export default function Contact({ closeBTN, listing, userRef }) {
+    const [message, setMessage] = useState('');
+    const [agentData, setAgentData] = useState(null);
+
+    const onChange = (e) => {
+        setMessage(e.target.value);
+    }
+
+    useEffect(() => {
+        async function fetchAgentData() {
+            const docRef = doc(db, "users", userRef);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setAgentData(docSnap.data())
+            } else {
+                toast.error("Opps! Error fetching Agent's contact.");
+            }
+        }
+        fetchAgentData();
+    }, [userRef])
+
+    return (
+        <div>
+            {agentData !== null && (
+                <>
+                    <button className="absolute top-0 right-0 block" onClick={closeBTN}>
+                        <IoCloseCircleOutline className="text-red-600 text-3xl" />
+                    </button>
+                    <p className="block text-gray-500 mt-0">You're about sending a mail to <span className="text-purple-600 font-bold">{agentData.username.charAt(0).toUpperCase() + agentData.username.slice(1)}</span> agent concerning this listing.</p>
+                    <textarea
+                        className="w-full ring-offset-inherit bg-[#ffeeb1] border-green-500 focus:border-purple-400 text-gray-600"
+                        name="message" id="" rows="3"
+                        onChange={onChange}
+                        value={message}
+                    ></textarea>
+                    <a
+                        href={`mailto:${agentData.email}?Subject=${listing?.name}&body=${message}`}
+                        className="block border-2 border-green-500 text-green-500 hover:bg-green-600 hover:text-white active:bg-green-600 active:text-white font-bold py-2 px-5 w-full rounded transition text-center ease-in-out duration-300 uppercase"
+                    >
+                        Send Message
+                    </a>
+                </>
+            )}
+        </div>
+    )
+}
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -16,11 +16,11 @@ import {
 import "swiper/swiper-bundle.css";
 import Loader from "../components/Loader";
 import { ImLocation2 } from "react-icons/im";
-import { IoCloseCircleOutline } from "react-icons/io5";
 import Moment from "react-moment";
 import { toast } from "react-toastify";
 import { GrEdit } from "react-icons/gr";
 import { BsFillTrash3Fill } from "react-icons/bs";
+import Contact from "../components/Contact";
 
 export default function Listing() {
   const nav = useNavigate();
@@ -29,8 +29,6 @@ export default function Listing() {
   const [isLoading, setisLoading] = useState(true);
   const [shareLink, setShareLink] = useState(false);
   const [contactAgent, setContactAgent] = useState(false);
-  const [agentData, setAgentData] = useState(null);
-  const [message, setMessage] = useState('');
   const param = useParams();
 
   const formatPrice = (price) => {
@@ -43,10 +41,6 @@ export default function Listing() {
 
   const contactForm = () => {
     setContactAgent(true);
-  }
-
-  const onChange = (e) => {
-    setMessage(e.target.value);
   }
   
   const closeBTN = () => {
@@ -67,18 +61,7 @@ export default function Listing() {
     fetchListing();
   }, [param.listingId]);
 
-  useEffect(() => {
-    async function fetchAgentData() {
-      const docRef = doc(db, "users", listing?.userRef);
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        setAgentData(docSnap.data())
-      }else{
-        toast.error("Opps! Error fetching Agent's contact.");
-      }
-    }
-    fetchAgentData();
-  }, [listing?.userRef])
+  
   
   const onEdit = (listingId) =>  {
     nav(`/profile/listing/${listingId}/edit`);
@@ -293,21 +276,14 @@ export default function Listing() {
             )}
             {contactAgent && (
               <div className="relative pt-7">
-                <button className="absolute top-0 right-0 block" onClick={closeBTN}> <IoCloseCircleOutline className="text-red-600 text-3xl"/> </button>
-                <p className="block text-gray-500 mt-0">You're about sending a mail to <span className="text-purple-600 font-bold">{agentData.username.charAt(0).toUpperCase() + agentData.username.slice(1)}</span> agent concerning this listing.</p>
-                <textarea 
-                  className="w-full ring-offset-inherit bg-[#ffeeb1] border-green-500 focus:border-purple-400 text-gray-600" 
-                  name="message" id="" rows="3" 
-                  onChange={onChange}
-                  value={message}
-                  >
-                  </textarea>
-                <a 
-                  href={`mailto:${agentData.email}?Subject=${listing.name}&body=${message}`} 
-                  className="block border-2 border-green-500 text-green-500 hover:bg-green-600 hover:text-white active:bg-green-600 active:text-white font-bold py-2 px-5 w-full rounded transition text-center ease-in-out duration-300 uppercase"
-                >
-                  Send Message
-                </a>
+                <Contact 
+                  listing={listing.data}
+                  agentData={listing.userRef}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                  userRef={listing.userRef}
+                  closeBTN={closeBTN}
+                />
               </div>
             )}
           </div>
