5a34b0336ce9001f7374bdba5ded7dff7e7a1095
Yakubu Josiah <jossysmothest007@gmail.com>
2024-01-29 21:18:38 +0100

    feat: Improved UX by adding validation and feedback for email input

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -4,28 +4,39 @@ import { Link } from "react-router-dom";
 import { HiUser } from "react-icons/hi";
 import { MdVerifiedUser } from "react-icons/md";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function ForgotPassword() {
+  const auth = getAuth();
+  const [email, setEmail] = useState("");
   const [form, setForm] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  function handleChage(e) {
-    setEmail(e.target.value);
-  }
+  const [errorMessage, setErrorMessage] = useState("");
   const [formSuccess, setformSuccess] = useState(false);
-  const auth = getAuth();
-  const onForgot = async (e) => {
+
+  function handleChange(e) {
+    setEmail(e.target.value);
+    setErrorMessage(false);
+  }
+  
+  const passwordResetEmail = async (e) => {
     e.preventDefault();
+    if(!email){
+      setErrorMessage("Please enter your email address.");
+      return;
+    }
     try {
+      setErrorMessage(false);
       setIsLoading(true);
       await sendPasswordResetEmail(auth, email);
       setForm(false);
       setformSuccess(true);
-      console.log("Email Sent!!");
+      toast.success("Email sent successfully!");
     } catch (error) {
-      console.log("Couldn't send reset password");
+      setErrorMessage("Invalid email address");
+    } finally {
+      setIsLoading(false);
     }
-    console.log("Cart Witnessing !!");
   };
 
   return (
@@ -34,23 +45,28 @@ export default function ForgotPassword() {
         <div className="lg:flex-1 mx-auto md:flex-col w-full my-5 formCont">
           <div className="form w-3/5 p-7 mx-auto text-green-900 font-semibold shadow-thick shadow-md min-w-[280px]">
             <h1 className="text-3xl pb-3 mt-10">Forgot Password?</h1>
-            <p className="text-sm ">
+            <p className="text-sm">
               No problem. Just let us know your email address and we will email
               you a password reset link that will allow you to choose a new one.
             </p>
-            <form onClick={onForgot}>
+            <form onSubmit={passwordResetEmail}>
               <div className="relative">
-                <span className="absolute left-5 bottom-3 text-xl">
+                  <span className={`absolute left-5 bottom-3 text-xl ${errorMessage ? "lg:top-[3.1rem] md:top-[3.2rem] top-[3.5rem] text-red-500" : ""}`}>
                   <HiUser />
                 </span>
                 <input
                   type="email"
                   id="email"
-                  className="p-2 pl-12 w-full mt-10 rounded-full"
+                  className={`p-2 pl-12 w-full mt-10 rounded-full ${
+                    errorMessage ? "border-red-500 text-red-500 focus:border-0": ""
+                  }`} 
                   value={email}
-                  onChange={handleChage}
+                  onChange={handleChange}
                   placeholder="Enter Email Address"
                 />
+                {errorMessage && (
+                  <p className="text-red-500 text-sm mt-2 font-light pl-3">{errorMessage}</p>
+                )}
               </div>
               <>
                 {isLoading ? (
@@ -61,7 +77,8 @@ export default function ForgotPassword() {
                     </div>
                   </button>
                 ) : (
-                  <button className="border p-2 w-full mt-7 rounded-full hover:shadow-lg hover:text-white transition duration-300 ease-in-out px-4 py-2">
+                  <button className="border p-2 w-full mt-7 rounded-full hover:shadow-lg hover:text-white transition duration-300 ease-in-out px-4 py-2"
+                  >
                     Reset Password
                   </button>
                 )}
@@ -95,9 +112,9 @@ export default function ForgotPassword() {
               </h1>
             </div>
             <p className="text-sm lg:text-lg">
-              Before continuing, could you set a new password by clicking on the
-              link we just emailed to you. If you didn't receive the email, we
-              will gladly send you another.
+              Before you proceed, please take a moment to set a new password by clicking on the link we've just sent to your email. 
+              In case you don't see it in your inbox, no worries - we're happy to send it again. 
+              Check your spam folder or simply request another reset, and you'll be back in action in no time!
             </p>
           </div>
         </div>
