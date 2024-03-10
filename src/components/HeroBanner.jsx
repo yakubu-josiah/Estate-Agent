9fb9a14cf3cd99dbb5465f41c1cec0f89e286ca1
Yakubu Josiah <jossysmothest007@gmail.com>
2024-03-10 23:31:43 +0100

    Enhancement: Implemented alternative approach for fetching owner names in listings

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { doc, collection, getDocs, getDoc, query, orderBy, limit } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import Loader from "../components/Loader";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import {
-    Navigation,
-    Pagination,
-    Scrollbar,
     A11y,
     EffectFade,
     Autoplay,
@@ -19,6 +16,7 @@ export default function HeroBanner() {
     const nav = useNavigate();
     const [listings, setListings] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [agentName, setAgentName] = useState('');
 
     const formatPrice = (price) => {
         return price && price !== 0 ? (
@@ -28,25 +26,45 @@ export default function HeroBanner() {
         );
     };
 
-
-
     useEffect(() => {
-        async function fetchLising() {
-            const listingRef = collection(db, "listings");
-            const q = query(listingRef, orderBy("timestamp", "desc"), limit(5));
-            const querySnap = await getDocs(q);
-            let listings = [];
-            querySnap.forEach((doc) => {
-                return listings.push({
-                    id: doc.id,
-                    data: doc.data(),
+        async function fetchListings() {
+            try {
+                const listingRef = collection(db, "listings");
+                const q = query(listingRef, orderBy("timestamp", "desc"), limit(5));
+                const querySnap = await getDocs(q);
+                const fetchedListings = [];
+
+                for (const doc of querySnap.docs) {
+                    const listingData = doc.data();
+                    fetchedListings.push({
+                      id: doc.id,
+                      data: listingData,
+                      userRef: listingData.userRef, 
+                    });
+                }
+
+                const userDocRefs = fetchedListings.map(listing => doc(db, "users", listing.userRef));
+                const userDocs = await Promise.all(userDocRefs.map(getDoc));
+                
+                const listingsWithOwners = fetchedListings.map((listing, index) => {
+                const userData = userDocs[index].data();
+                console.log(userData);
+                return {
+                    ...listing,
+                    agentName: userData.username, 
+                };
                 });
-            });
-            setListings(listings);
-            setIsLoading(false);
+
+                setListings(listingsWithOwners);
+                setAgentName(agentName);
+                setIsLoading(false);
+                
+            } catch (error) {
+                console.log(error);
+            }
         }
-        fetchLising();
-    }, []);
+        fetchListings();
+    }, [agentName]);
 
     if (isLoading) {
         return <Loader />;
@@ -64,32 +82,25 @@ export default function HeroBanner() {
             <>
                 <Swiper
                     modules={[
-                        Navigation,
-                        Scrollbar,
-                        Pagination,
                         A11y,
                         EffectFade,
                         Autoplay,
                     ]}
                     slidesPerView={1}
-                    navigation
                     effect="fade"
                     autoplay={{ delay: 4000 }}
-                    pagination={{ clickable: true }}
-                    scrollbar={{ draggable: true }}
                 >
-                    {listings.map(({ data, id }) => (
+                    {listings.map(({ data, id, agentName }) => (
                         <SwiperSlide key={id} onClick={() => exploreMore(id)}>
                             <div
                                 style={{
                                     background: `url(${data.images[0]}) center no-repeat`,
                                     backgroundSize: "cover",
                                     width: "100%",
-                                    height: "300px",
                                 }}
-                                className="relative w-full h-[300px]"
+                                className="herobanner w-full h-[300px] "
                             >
-                            <p className="absolute text-purple-700 right-0 top-2 font-bold px-2 rounded-l-full max-w-[90%] bg-[#fff7d1] shadow-lg opacity-80 ">{data.name}</p>
+                            <p className="absolute text-purple-700 right-0 top-2 font-bold px-2 rounded-l-full max-w-[90%] bg-[#f1de7e] shadow-lg opacity-80 ">{agentName}</p>
                             <p className="absolute text-purple-700 left-0 bottom-2 font-bold px-2 rounded-r-full max-w-[90%] bg-[#fff7d1] shadow-lg opacity-80 ">{data.sale ? ( <span> Sale: {formatPrice(data.sale)} </span> ) : ( <span> Lease: {formatPrice(data.lease)} </span>)}</p>
                             </div>
                         </SwiperSlide>
